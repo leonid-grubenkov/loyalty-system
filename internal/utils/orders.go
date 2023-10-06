4ec3846a1fc923796e1f7251104a28c37ab7ac65
Леонид Грубенков <leonid@MacBook-Pro-Leonid.local>
2023-10-06 18:13:39 +0300

    add Luhn algorithm check

--- internal/utils/orders.go
+++ internal/utils/orders.go
@@ -8,5 +8,31 @@ func ParseOrder(body string) int {
 	if err != nil {
 		return -1
 	}
+
+	if !isValidLuhn(body) {
+		return -1
+	}
+
 	return num
+}
+
+func isValidLuhn(s string) bool {
+	sum := 0
+	alternate := false
+
+	for i := len(s) - 1; i >= 0; i-- {
+		digit := int(s[i] - '0')
+
+		if alternate {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+
+		sum += digit
+		alternate = !alternate
+	}
+
+	return sum%10 == 0
 }
